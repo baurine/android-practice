7e98711e8853d75c16965bb875f45843ee1e30f4
baurine <2008.hbl@gmail.com>
2015-07-07 16:53:42 +0800

    remove TabLayout

--- designsupportlibdemo/src/main/java/com/baurine/designsupportlibdemo/MainActivity.java
+++ designsupportlibdemo/src/main/java/com/baurine/designsupportlibdemo/MainActivity.java
@@ -1,12 +1,11 @@
 package com.baurine.designsupportlibdemo;
 
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewGroup mLayoutContentRoot;
     private FloatingActionButton mFabBtn;
     private Toolbar mToolbar;
-    private TabLayout mTabLayout;
+//    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setupFabBtn();
         setupToolbar();
         setupDrawerLayout();
-        setupTabLayout();
+//        setupTabLayout();
     }
 
     private void findViews() {
@@ -45,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         mLayoutContentRoot = (ViewGroup) findViewById(R.id.layout_content_root);
         mFabBtn = (FloatingActionButton) findViewById(R.id.fab_btn);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        mTabLayout = (TabLayout) findViewById(R.id.tablayout);
+//        mTabLayout = (TabLayout) findViewById(R.id.tablayout);
     }
 
     private void setupFabBtn() {
@@ -83,11 +82,13 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout.setDrawerListener(toggle);
     }
 
+    /*
     private void setupTabLayout() {
         mTabLayout.addTab(mTabLayout.newTab().setText("Tab 1"));
         mTabLayout.addTab(mTabLayout.newTab().setText("Tab 2"));
         mTabLayout.addTab(mTabLayout.newTab().setText("Tab 3"));
     }
+    */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
