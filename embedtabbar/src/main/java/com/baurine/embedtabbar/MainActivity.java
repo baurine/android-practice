be7e290f4519254a4780aa3c9800b4ee38f62a62
baurine <2008.hbl@gmail.com>
2015-07-26 20:55:47 +0800

    加上 Tab

--- embedtabbar/src/main/java/com/baurine/embedtabbar/MainActivity.java
+++ embedtabbar/src/main/java/com/baurine/embedtabbar/MainActivity.java
@@ -4,22 +4,65 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
     private TabContentPagerAdapter mPagerAdapter;
+    private ActionBar mActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mActionBar = getSupportActionBar();
+        setupViewPager();
+        setupActionBar();
+    }
+
+    private void setupViewPager() {
         mPagerAdapter = new TabContentPagerAdapter(getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mPagerAdapter);
+
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                mActionBar.setSelectedNavigationItem(position);
+            }
+        });
+    }
+
+    private void setupActionBar() {
+        mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.TabListener listener = new ActionBar.TabListener() {
+            @Override
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+
+            @Override
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+        };
+
+        for (int i = 0; i < 3; i++) {
+            mActionBar.addTab(mActionBar.newTab()
+                    .setText("Tab " + (i + 1))
+                    .setTabListener(listener));
+        }
     }
 
     public static class TabContentPagerAdapter extends FragmentStatePagerAdapter {
