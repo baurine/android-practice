6c1274910e6cff8fea121e355b62e74fba4fa4cc
Baurine Huang <2008.hbl@gmail.com>
2015-08-19 18:28:14 +0800

    implement blur effect

--- retrofitdemo/src/main/java/com/baurine/retrofitdemo/adapter/FlowerAdapter.java
+++ retrofitdemo/src/main/java/com/baurine/retrofitdemo/adapter/FlowerAdapter.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import com.baurine.retrofitdemo.R;
 import com.baurine.retrofitdemo.model.Flower;
+import com.baurine.retrofitdemo.view.BlurTransformation;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -33,30 +34,31 @@ public class FlowerAdapter extends ArrayAdapter<Flower> {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_flower, parent,
                     false);
-            ImageView imageView = (ImageView) convertView.findViewById(R.id.iv_flower);
-            TextView textView = (TextView) convertView.findViewById(R.id.tv_name);
-            viewHolder = new ViewHolder(imageView, textView);
+            viewHolder = new ViewHolder(convertView);
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
 
         viewHolder.mTvName.setText(flower.getName());
+
         Picasso.with(getContext())
                 .load(URL + flower.getPhoto())
-                .resize(200, 200)
+                .transform(new BlurTransformation(getContext(), 25))
                 .into(viewHolder.mIvImage);
 
         return convertView;
     }
 
     public static class ViewHolder {
+        public ViewGroup mLlContent;
         public ImageView mIvImage;
         public TextView mTvName;
 
-        public ViewHolder(ImageView imageView, TextView textView) {
-            this.mIvImage = imageView;
-            this.mTvName = textView;
+        public ViewHolder(View convertView) {
+            this.mLlContent = (ViewGroup) convertView.findViewById(R.id.ll_content);
+            this.mIvImage = (ImageView) convertView.findViewById(R.id.iv_flower);
+            this.mTvName = (TextView) convertView.findViewById(R.id.tv_name);
         }
     }
 }
