e5d8b4d6838349d533c79a81a5eba94e7ddd2389
Baurine Huang <2008.hbl@gmail.com>
2015-08-21 17:52:01 +0800

    implement basic music play

--- musicplayer/src/main/java/com/baurine/musicplayer/MainActivity.java
+++ musicplayer/src/main/java/com/baurine/musicplayer/MainActivity.java
@@ -1,37 +1,122 @@
 package com.baurine.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String MP3_URL = "http://192.168.0.215:3000" +
+            "/uploads/track/file/20/low_quality_4_masazumi_ozawa_attraction.mp3";
+
+    private MediaPlayer mediaPlayer;
+
+    private EditText edtxUrl;
+
+    private boolean prepared = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupButtons();
+        edtxUrl = (EditText) findViewById(R.id.edtx_url);
+        edtxUrl.setText(MP3_URL);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onDestroy() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+        }
+        super.onDestroy();
+    }
+
+    private void setupButtons() {
+        Button btnStart, btnPlayPause, btnStop;
+        btnStart = (Button) findViewById(R.id.btn_start);
+        btnPlayPause = (Button) findViewById(R.id.btn_play_pause);
+        btnStop = (Button) findViewById(R.id.btn_stop);
+
+        btnStart.setOnClickListener(this);
+        btnPlayPause.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_start:
+                startPlay();
+                break;
+            case R.id.btn_play_pause:
+                pausePlay();
+                break;
+            case R.id.btn_stop:
+                stopPlay();
+                break;
+            default:
+                break;
+        }
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void startPlay() {
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
         }
 
-        return super.onOptionsItemSelected(item);
+        mediaPlayer.reset();
+        prepared = false;
+        try {
+            mediaPlayer.setDataSource(edtxUrl.getText().toString());
+            mediaPlayer.prepareAsync();
+//            mediaPlayer.start();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                prepared = true;
+                mp.start();
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                if (mediaPlayer != null) {
+                    mediaPlayer.release();
+                    mediaPlayer = null;
+                }
+            }
+        });
+    }
+
+    private void pausePlay() {
+        if (mediaPlayer != null && prepared) {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.pause();
+            } else {
+                mediaPlayer.start();
+            }
+        }
+    }
+
+    private void stopPlay() {
+        if (mediaPlayer != null && prepared) {
+            mediaPlayer.stop();
+        }
     }
 }
